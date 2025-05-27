a1a310bb5ded1a6ea5c3420250c2b3ed65c49ac6
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-05-27 18:14:41 -0300

    Comentando app/serializers.py

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,8 @@
-from app.models import Todo
-from rest_framework import serializers
+from app.models import Todo            # Importa o modelo Todo para ser serializado
+from rest_framework import serializers # Importa o módulo serializers do Django REST Framework para criação dos serializers
 
+# Serializer para o modelo Todo, responsável por converter objetos Todo para JSON e vice-versa
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Todo
-        fields = ['id', 'name', 'done', 'created_at']
+        model = Todo                                   # Define o modelo que será serializado
+        fields = ['id', 'name', 'done', 'created_at']  # Define os campos do modelo que serão incluídos na serialização
