da56cfcd3b4b121cebdffc014f230a586b739442
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-05-27 18:10:46 -0300

    Comentando  app/apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,5 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # Importa a classe base AppConfig do Django para configurar apps
 
+# Classe de configuração do app 'app'
 class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    default_auto_field = 'django.db.models.BigAutoField' # Define o tipo padrão da chave primária para os modelos deste app como BigAutoField (inteiro grande autoincremental)
+    name = 'app'     # Nome do app (deve ser o mesmo nome da pasta do app)
