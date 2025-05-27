e3c4f870f16bde4a5d8326489b6847dd8348089b
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-05-27 18:22:01 -0300

    Comentando app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
-from app.views import TodoViewSet
-from rest_framework.routers import DefaultRouter
+from app.views import TodoViewSet                # Importa o ViewSet que define as operações CRUD para o modelo Todo
+from rest_framework.routers import DefaultRouter # Importa o roteador padrão do Django REST Framework que gera automaticamente rotas REST
 
+# Cria uma instância do roteador padrão
 router = DefaultRouter()
+
+# Registra o TodoViewSet no roteador na raiz ('') do caminho, ou seja, URLs começam com /todo/ (ou o que for configurado no include)
 router.register(r'', TodoViewSet)
 
+# O atributo 'urls' do roteador gera automaticamente todas as URLs para as operações CRUD do ViewSet
 urlpatterns = router.urls
